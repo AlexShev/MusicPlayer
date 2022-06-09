ee3596f497140581ac5764cae1837661936e7ea9
NeAndrew <65078011+Andrey1233-maker@users.noreply.github.com>
2022-06-09 13:45:01 +0300

    saturn

--- app/src/main/java/com/example/musicplayer/Activities/PlayMusicActivity.java
+++ app/src/main/java/com/example/musicplayer/Activities/PlayMusicActivity.java
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageButton;
 
+import com.example.musicplayer.Components.MusicLikeButton;
+import com.example.musicplayer.Components.MusicPlayModeButton;
 import com.example.musicplayer.R;
 
 public class PlayMusicActivity extends AppCompatActivity {
 
     private ImageButton _circleButton;
+    private ImageButton _loveButton;
 
 
     private MusicPlayModeButton musicPlayModeButton;
+    private MusicLikeButton musicLikeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +25,8 @@ public class PlayMusicActivity extends AppCompatActivity {
 
         this._circleButton = this.findViewById(R.id.circle_button_play);
         musicPlayModeButton = new MusicPlayModeButton(this._circleButton);
+
+        this._loveButton = this.findViewById(R.id.music_like_play);
+        musicLikeButton = new MusicLikeButton(this._loveButton, false);
     }
 }
